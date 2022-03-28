d758a76c0d287b042059cbe3a0f5d099f9a495f7
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2022-03-28 22:04:12 +0300

    card data is starting to shape

--- src/components/cards/MainCard.jsx
+++ src/components/cards/MainCard.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import weatherApiKey from '../../apis/weatherApiKey'
 import { getLocation } from '../../rtk/countriesSlice'
+import { getDate } from '../../rtk/dateSlice'
 
 const MainCard = () => {
   const lat1 = useSelector(getLocation)
@@ -10,6 +11,10 @@ const MainCard = () => {
   const lon1 = useSelector(getLocation)
   const lon = lon1.payload.countries.currentLocation.lat
   const part = ''
+
+  const date1 = useSelector(getDate)
+  const date = date1.payload.date.date
+  console.log('date in card', date)
 
   const fetchWeather = async () => {
     const request = await fetch(
@@ -24,7 +29,12 @@ const MainCard = () => {
   }, [lat1])
   return (
     <>
-      <div className='bg-success main-card'></div>
+      <div className='bg-success main-card'>
+        <p>date: {date}</p>
+        <p>lat {lat}</p>
+        <p>lon {lon}</p>
+        <p></p>
+      </div>
     </>
   )
 }
