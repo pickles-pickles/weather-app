9b951da5442d41293a3e768846682aa77f97abcd
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2022-03-28 21:36:04 +0300

    calendar ranged: fixed

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import { addDays } from 'date-fns'
 import { useDispatch } from 'react-redux'
 import { setDate } from '../../rtk/dateSlice'
 
@@ -20,6 +21,8 @@ const Calendar = () => {
             console.log('selected date', startDate)
             dispatch(setDate(date.toString()))
           }}
+          maxDate={addDays(new Date(), 8)}
+          minDate={addDays(new Date(), 0)}
         >
           <div style={{ color: 'red' }}>Don't forget to check the weather!</div>
         </DatePicker>
