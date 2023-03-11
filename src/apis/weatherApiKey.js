e0755f0aabb68420a7b275f6b7168f33d630ddcc
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-03-11 20:41:05 +0200

    hide-api-key-for-local-development

--- src/apis/weatherApiKey.js
+++ src/apis/weatherApiKey.js
@@ -1,3 +1,3 @@
 //const weatherApiKey = '324807493f0dd04f3af3809dacdceade'
-const weatherApiKey = '1e29088810d9e80c9fff18b7f3fc9623'
+const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY
 export default weatherApiKey
