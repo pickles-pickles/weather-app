18ac040de8ac6cb48a7fd6a05411054b06ec8b56
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-03-11 20:54:27 +0200

    some-playing

--- src/apis/weatherApiKey.js
+++ src/apis/weatherApiKey.js
@@ -1,3 +1,3 @@
 //const weatherApiKey = '324807493f0dd04f3af3809dacdceade'
-const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY
+const weatherApiKey = '1e29088810d9e80c9fff18b7f3fc9623'
 export default weatherApiKey
