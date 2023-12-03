e3c85f18b4e1557cfbc16eb172ddafb103a4432d
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-12-03 21:12:59 +0200

    some cleanup and select logic refactoring

--- src/components/details/DetailsRange.jsx
+++ src/components/details/DetailsRange.jsx
@@ -5,35 +5,16 @@ import {
   setPlotEndDate,
   setPlotStartDate
 } from '../../rtk/dateSlice'
-import { getWeatherDaily, setMeanTemp } from '../../rtk/weatherSlice'
 
 const DetailsRange = () => {
   const dispatch = useDispatch()
   const start = useSelector(getPlotStartDate)
   const end = useSelector(getPlotEndDate)
-  const dailyWeather = useSelector(getWeatherDaily)
 
   const options = [0, 1, 2, 3, 4, 5, 6, 7]
   function handleChange (e, callback) {
     dispatch(callback(parseInt(e.target.value)))
   }
-
-  const calcMeanTemp = (s = start, e = end) => {
-    let temps = 0,
-      count = 0,
-      temp = 0
-    if (dailyWeather.length > 1) {
-      for (let i = s; i < e; i++) {
-        temp = dailyWeather[i].temp.day
-        temps += temp
-        count++
-      }
-      return temps / count
-    }
-    return undefined
-  }
-
-  let meanTempToSend = calcMeanTemp()
 
   return (
     <>
@@ -50,7 +31,14 @@ const DetailsRange = () => {
           }}
         >
           {options.map(option => (
-            <option value={option} key={option}>
+            <option
+              value={option}
+              key={option}
+              disabled={parseInt(option) > parseInt(end)}
+              className={`${
+                parseInt(option) > parseInt(end) && 'option-disabled'
+              }`}
+            >
               {option}
             </option>
           ))}
